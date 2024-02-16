e8f291932aa391eea6dbc4b09178a0946ba3b382
1711ashishsahu <ashishsahu76732@gmail.com>
2024-02-16 12:17:13 +0530

    Adding 1

---
+++ VarDatatypeObjects.js
@@ -0,0 +1,38 @@
+console.log("Hey this is tutorial 55");
+//variable(var)
+var a = 5;
+var b = 6;
+var c = "Harry";
+console.log(a+b+8);
+console.log(typeof a, typeof b, typeof c);
+
+//let
+let k = 4;
+{
+    let k = 55
+    console.log(k);
+}
+console.log(k);
+
+// Datatype
+let x = "Ashish bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x, y, z, p, q, r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+
+// objects
+let o = {
+    name: "Harry",
+    "job code": 5678,
+    "is_handsome": true
+}
+console.log(o);
+console.log(typeof o);
+o.salary = "100 crores";
+console.log(o);
+o.salary = "500 crores";
+console.log(o);
