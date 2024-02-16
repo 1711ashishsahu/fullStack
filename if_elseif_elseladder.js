319e8cad29bad5863ff7d3d39196d38c904e70ec
1711ashishsahu <ashishsahu76732@gmail.com>
2024-02-16 13:35:42 +0530

    Adding 2

---
+++ if_elseif_elseladder.js
@@ -0,0 +1,27 @@
+console.log("Hello I am a conditional tutorial");
+
+// let age = 0;
+let age = 1;
+let grace = 2;
+// if((age-grace)>18){
+if(age>18){
+    console.log("yor can Drive");
+}
+else if(age == 0){
+    console.log("Are you kidding");
+}
+else if(age == 1){
+    console.log("Are you again kidding");
+}
+else{
+    console.log("you cannot Drive");
+}
+console.log(age + grace);
+
+//ternary operator
+
+x = 6
+y = 8
+let z = x > y ? (x+y):(x-y)
+
+console.log(z);
