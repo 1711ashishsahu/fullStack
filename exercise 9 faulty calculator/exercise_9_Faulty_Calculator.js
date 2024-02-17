59cb737a21632cbe521ea2c481d6be32fb6f6112
1711ashishsahu <ashishsahu76732@gmail.com>
2024-02-17 22:30:06 +0530

    Adding 5

---
+++ exercise 9 faulty calculator/exercise_9_Faulty_Calculator.js
@@ -0,0 +1,34 @@
+/* Create a faulty calculator using javascript
+
+
+This faulty calculator does following:
+1) It takes two numbers as input from the user
+2) It performs wrong operations as follows:
++ ---> -
+* ---> +
+- ---> /
+/ ---> **
+
+note --> It performs wrong operations 10% of the times
+*/
+
+let firstNum = prompt("First number:")
+let operation = prompt("Enter Operation")
+let secondNum = prompt("Second number:")
+let random = Math.random()
+console.log(random);
+let obj = {
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "**"
+}
+
+if (random>0.1) {
+    console.log(`The result is ${eval(`${firstNum} ${operation} ${secondNum}`)}`);
+    alert(`The result is ${eval(`${firstNum} ${operation} ${secondNum}`)}`);
+}
+else{
+    operation = obj[operation]
+    alert(`The result is ${eval(`${firstNum} ${operation} ${secondNum}`)}`);
+}
