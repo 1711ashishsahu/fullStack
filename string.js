d54667cfa34173fc522a933fc276ccfa7657c8da
1711ashishsahu <ashishsahu76732@gmail.com>
2024-02-18 00:21:08 +0530

    Adding 6

---
+++ string.js
@@ -0,0 +1,27 @@
+console.log("This is string tutorial");
+
+let a = "HarryBhai";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[5]);
+console.log(a[8]);
+console.log(a[9]);
+
+console.log(a.length);
+
+let somename = "Harry"
+let friend = "Rohan"
+console.log("His name is ", somename, "and his friends name is ", friend);
+console.log(`His name is ${somename} and his friends name is ${friend}`);
+
+let b = "Ashish Bhai";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.slice(1,5));
+console.log(b.slice(1));
+console.log(b.replace("Bhai", "Bhau"));
+console.log(b.concat(a));
+console.log(b.concat(a, "Nirmal", "Sahil"));
+console.log(b.trim());
+
