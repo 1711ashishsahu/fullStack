967579c504c4ea3fb35db72dd89c08b42fbd7f12
1711ashishsahu <ashishsahu76732@gmail.com>
2024-02-21 19:39:40 +0530

    Adding 12

---
+++ DOM/script.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+// video 67 Children, Parent, Sibling Nodes
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children
+
+document.body.firstElementChild.children
+document.body.firstElementChild.children[0]
+document.body.firstElementChild.children[3]
+document.body.firstElementChild.children[4]
+document.body.firstElementChild.children[3].nextElementSibling
+document.body.firstElementChild.children[3].previousElementSibling
+document.body.firstElementChild.children[3].previousSibling
+document.body.firstElementChild.children[3].parentElement
+
+document.body.children
+document.body.children[1]
+
+document.body.children[1].nextElementSibling
+document.body.children[1].previousElementSibling
