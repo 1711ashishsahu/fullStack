6070a66c9c68e282071ca815b86421cb4823a3fd
1711ashishsahu <ashishsahu76732@gmail.com>
2024-02-22 13:59:33 +0530

    Adding 12.2

--- DOM/Exercise_12/script.js
+++ DOM/Exercise_12/script.js
@@ -1,5 +1,14 @@
-document.getElementById("red").style.backgroundColor = "red"
-document.getElementById("blue").style.backgroundColor = "blue"
-document.getElementById("green").style.backgroundColor = "green"
-document.getElementById("yellow").style.backgroundColor = "yellow"
-document.getElementById("orange").style.backgroundColor = "orange"
+// let boxes = document.getElementsByClassName("box")
+let boxes = document.querySelector(".container").children
+
+function getRandomColor(){
+    let val1 = Math.ceil(0 + Math.random()* 255);
+    let val2 = Math.ceil(0 + Math.random()* 255);
+    let val3 = Math.ceil(0 + Math.random()* 255);
+    return `rgb(${val1}, ${val2}, ${val3})`
+}
+
+Array.from(boxes).forEach(e=>{
+    e.style.backgroundColor = getRandomColor()
+    e.style.color = getRandomColor()
+})
