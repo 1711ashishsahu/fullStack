f359b00b1ce9c003dd4862977b3e7c440ab681ce
1711ashishsahu <ashishsahu76732@gmail.com>
2024-03-11 15:32:50 +0530

    understanding the question properly

--- exercise_11_Factorial.js
+++ exercise_11_Factorial.js
@@ -1,17 +1,19 @@
 // Factorial --> The factorial of a number is the multiplication of all the numbers between one and the number itself.
 
+// let arr = Array.from(Array().keys())
+// console.log(arr);
+let fact = 6
+function factorial(number){
+    let arr = Array.from(Array(number+1).keys())
+    // console.log(arr.slice(1,));
+    let c = arr.slice(1,).reduce((a,b)=>{
+        return a*b
+    })
+    return c
+}
+console.log(factorial(fact));
 
-// let fact = 6
-// function factorial(number){
-//     let arr = Array.from(Array(number+1).keys())
-//     console.log(arr.slice(1,));
-//     let c = arr.slice(1,).reduce((a,b)=>{
-//         return a*b
-//     })
-//     console.log(c)
-// }
 
-// factorial(fact)
 // OR
 
 // let fact = 6
@@ -27,8 +29,8 @@
 let facto = 6
 function facFor(number){
     let fac = 1
-    for (let index = 1; index <= number; index++) {
-        fac = fac * index
+    for (let a = 1; a <= number; a++) {
+        fac = fac * a
     }
     return fac
 }
