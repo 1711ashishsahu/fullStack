cf3b8de64ed391a60d55179f581291c868857e1b
1711ashishsahu <ashishsahu76732@gmail.com>
2024-03-12 10:39:46 +0530

    inserting and removing elements

---
+++ DOM/inserting_and_removing_elements/script.js
@@ -0,0 +1,21 @@
+document.querySelector(".container")​
+document.querySelector(".container").innerText
+document.querySelector(".container").outerHTML
+document.querySelector(".container").outerText
+document.querySelector(".container").tagName
+document.querySelector(".box").tagName
+document.querySelector(".box").nodeName
+document.querySelector(".box").nodeType
+document.querySelector(".box").textContent
+document.querySelector(".container").textContent
+
+document.querySelector(".box").hidden
+// false
+document.querySelector(".box").hidden = true
+// true
+document.querySelector(".container").hidden = true
+
+document.querySelector(".box").setAttribute("style", "color: green")
+
+document.querySelector(".box").setAttribute("style", "background-color: yellow")
+document.querySelector(".box").dataset
