bacb6e6b2c395412c267447338cb6cc3adb548f8
1711ashishsahu <ashishsahu76732@gmail.com>
2024-03-30 16:14:58 +0530

    callback function

---
+++ DOM/CallBack_promise_async_await/script.js
@@ -0,0 +1,25 @@
+// console.log("Harry is a Hacker");
+// console.log("Rohan is a Hecker");
+
+// setTimeout(() => {
+//     console.log("I am inside set-timeout");
+// }, 2000);
+// setTimeout(() => {
+//     console.log("I am inside set-timeout1");
+// }, 1000);
+
+// console.log("The End");
+
+// callback function
+const roman = (arg)=>{
+    console.log(arg);
+}
+const loadScript = (src, roman) => {
+    let sc = document.createElement("script");
+    sc.src = src;
+    // sc.onload = roman("Roman Ranges")
+    sc.onload = roman("Roman Ranges");
+    document.head.append(sc)
+}
+
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", roman)
